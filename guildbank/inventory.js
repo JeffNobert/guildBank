806952127c13162a4e8b5a7a07b484788d010686
Jeff Nobert <jeffrey.nobert@gmail.com>
2020-04-27 23:53:45 -0400

    Added charaters name

--- guildbank/inventory.js
+++ guildbank/inventory.js
@@ -1,10 +1,11 @@
 // inventory filterable by bankchars
-const query =  `select items.en, sum(inv.qty) as qty 
+const query =  `select chars.name, items.en, sum(inv.qty) as qty 
                 from inventory as inv 
                 join items on inv.item_id = items.id 
                 join chars on inv.char_id = chars.id
                 where chars.name like ?
-                group by item_id;`
+                group by chars.name, item_id 
+                order by chars.name, item.en;`
 
 const inventory = (args, message, db) => {
     char = '%'
